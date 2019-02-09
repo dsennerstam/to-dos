5cb4e3ffd4ef6d5ac9f9f34e93e62c21f78840c9
Daniel <daniel@balancedsolutions.se>
2019-02-09 14:19:24 +0100

    adding my projekt

---
+++ Assets/js/todos.js
@@ -0,0 +1,44 @@
+// Check of specific todos by clicking
+	// by makeing .completed in css we delete all 
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed")
+	});
+	//if li is grey
+// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+// 		// turn it black
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});	
+// 	}
+// 	// else 
+// 	else {
+// 		// turn it grey
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});	
+// 	}
+		
+// Click on X to delete Todo
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(1000, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+	
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+		//grabbing new todo text from imput
+		var todoText = $(this).val();
+		$(this).val("");
+		//creat new li in ul
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+	}
+})
+
+$("h1").on("click", ".fa-plus", function(){
+	$("input[type='text']").fadeToggle();
+});
